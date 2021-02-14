44cca4ddf846e84dbef82bf7f18a4713d0b1708e
alekseev <alekseev@aisen.com>
2021-02-14 14:05:43 +0300

    *

--- calc.py
+++ calc.py
@@ -1,10 +1,12 @@
 a = int(input("vvedite pervoe chislo: "))
 b = int(input("vvedite vtoroe chislo: "))
-c = input("vvedite znak operacii(+,-): ")
+c = input("vvedite znak operacii:(+,-,*) ")
 if c in ('+', '-', '*', '/'):
     if c == '+':
         print(a+b)
     elif c == '-':
         print(a-b)
+    elif c == '*':
+        print(a*b)
 else:
     print ("nepravilniy znak")
