5b23b2197ab822a8342912bb28eaedf3ddba2fa9
alekseev <alekseev@aisen.com>
2021-02-14 13:58:43 +0300

    +

---
+++ calc.py
@@ -0,0 +1,7 @@
+a = int(input("vvedite pervoe chislo: "))
+b = int(input("vvedite vtoroe chislo: "))
+c = input("vvedite znak operacii(+): ")
+if c == '+':
+    print(a+b)
+else:
+    print ("nepravilniy znak")
